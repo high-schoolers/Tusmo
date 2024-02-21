1b8bf1e38ad6e148419f561e233c0da6486a87b2
Ouumi <137509262+Ouumi@users.noreply.github.com>
2024-02-21 15:49:58 +0100

    Update presentation_regles
    La fonction difficultés renvoie directement le nombre de tours au lieu du niveau de difficulté

--- presentation_regles.py
+++ presentation_regles.py
@@ -1,16 +1,13 @@
 from time import*
 
-
 def intro():
     print("Bonjour! Bienvenue dans le TUSMO. Avez-vous besoin des règles?")
     sleep(0.5)
     ans = str(input("Oui ou Non? "))
     if ans == "oui" or ans == "Oui" or ans == "OUI":
-        print("Le TUSMO, aussi connu sous le nom de Wordle, est un jeu où "
-              "l'objectif est de deviner un mot.")
+        print("Le TUSMO, aussi connu sous le nom de Wordle, est un jeu où l'objectif est de deviner un mot.")
         sleep(0.5)
-        print("Vous commencez par choisir la longueur du mot, puis vous "
-              "mettez votre réponse.")
+        print("Vous commencez par choisir la longueur du mot, puis vous mettez votre réponse.")
         sleep(0.5)
         print("La lettre sera grise si elle n'est pas présente dans le mot.")
         sleep(0.5)
@@ -18,33 +15,39 @@ def intro():
         sleep(0.5)
         print("Elle sera bleue si elle est au bon endroit.")
 
-
 def difficultes():
     print("Il y a différentes difficultés :")
     sleep(0.5)
-    print("Facile : 10 tentatives")
+    print("Facile (1) : 10 tentatives")
     sleep(0.5)
-    print("Moyen : 6 tentatives")
+    print("Moyen (2) : 6 tentatives")
     sleep(0.5)
-    print("Difficile : 4 tentatives")
+    print("Difficile (3) : 4 tentatives")
     sleep(0.5)
-    print("Hardcore : 4 tentatives. Vous êtes forcé de garder les lettres "
-          "déjà trouvées.")
+    print("Hardcore (4) : 4 tentatives. Vous êtes forcé de garder les lettres déjà trouvées.")
     sleep(0.5)
-    while True:
+    diff = int(input("Choisissez 1,2,3 ou 4 : "))
+    if diff == 1:
+        tours = 10
+    elif diff == 2:
+        tours = 6
+    elif diff == 3 or diff == 4:
+        tours = 4
+    else :
         diff = int(input("Choisissez 1,2,3 ou 4 : "))
-        if diff == 1 or 2 or 3 or 4:
-            break
-    return diff
+    return tours
+
+
+from fonctions import mot_correct, lettre_correcte
 """
 def jeu(tours):
     while True:
         print("Tentatives restantes : ",tours)
         guess = str(input("Ecrivez un mot : ")
-        #if mot_correct(guess) == True:
-            #print("Gagné! :)")
-            #print("Tours restants -> ",tours)
-            #break
+        if mot_correct(guess) == True:
+            print("Gagné! :)")
+            print("Tours restants -> ",tours)
+            break
         else:  
             tours -= 1
             if tours == 0:
@@ -67,3 +70,5 @@ while True:
     else:
         print("UwU")
 """
+
+
