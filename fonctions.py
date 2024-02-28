23185815fce4f15aaedc12daf96d76af004cab5e
chhaahh <147736801+chhaahh@users.noreply.github.com>
2024-02-28 16:09:12 +0100

    fichier avec les fonctions du tusmo
    rajout de la fonction pour dessiner les essais du joueur et modification de lettre correcte ou mot correct (jesaisplus) car plus besoin de nb

--- fonctions.py
+++ fonctions.py
@@ -45,15 +45,13 @@ def mot_correct(mot, solution):
     True si l'utilisateur trouve le bon mot
     False si l'utilisateur n'as pas trouvé le mot
     """
-    global nb
     cnt_correct = 0     # le nombre de lettres bien placées
     for i in range(len(mot)):
         lettre = lettre_correcte(mot, i, solution)
         print(mot[i], ":", lettre)
         if lettre == 2:
             cnt_correct += 1
-        nb.append(lettre)
-    print(nb)    return cnt_correct == len(mot)   
+    return cnt_correct == len(mot)   
 
 def diff4(solution,guess,t):
     for i in range(len(solution)):
@@ -72,3 +70,52 @@ def diff_check(guess,solution,t):
             if guess[i] != solution[i]:
                 good = False
     return good
+
+def dessiner_tentative(tentatives, solution, taille, diff):
+    """Affiche les tentatives du joueur dans la fenetre Pygame
+
+    Paramètres
+    ----------
+    tentatives : liste de string
+        les tentatives du joueur 
+    solution : string
+        le mot correct
+    taille : int
+        longueur du mot à trouver
+    diff : int
+        nombre de tentatives autorisées en fonction de la difficulté du jeu
+
+    Retourne
+    --------
+    dessine les tentatives de l'utilisateur sur la fenêtre Pygame,
+    en affichant les lettres correctes, mal placées et bien placées
+    """
+    fenetre.fill((0, 0, 0))
+    largeur_case = largeur_fen // taille
+    largeur_bordure = 2
+    police = pygame.font.Font(None, 36)
+
+    for idx, t in enumerate(tentatives):
+        for i, lettre in enumerate(t):  # Utiliser enumerate pour obtenir l'indice correct
+            status = lettre_correcte(t, i, solution)  # Utiliser la lettre de la tentative
+            color = (255, 255, 255)  # Default color (white)
+
+            if status == 0:
+                color = (128, 128, 128)
+            elif status == 1:
+                color = (255, 255, 0)
+            elif status == 2:
+                color = (255, 0, 0)
+
+            texte_surface = police.render(lettre, True, (255, 255, 255))
+
+            case_x = i * largeur_case + i * largeur_bordure
+            case_rect = pygame.Rect(case_x, idx * (hauteur_fen // diff), largeur_case,
+                                    hauteur_fen // diff)
+
+            pygame.draw.rect(fenetre, color, case_rect)  # Utiliser la couleur correcte pour la case
+            pygame.draw.rect(fenetre, (255, 255, 255), case_rect, largeur_bordure)  # Dessiner la bordure
+            texte_rect = texte_surface.get_rect(center=case_rect.center)
+            fenetre.blit(texte_surface, texte_rect)
+
+    pygame.display.flip()
